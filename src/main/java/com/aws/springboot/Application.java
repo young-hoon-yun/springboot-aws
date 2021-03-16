a19ec049e242cdfe2365fec69fc3e780c8867e75
윤영훈 <https://github.com/young-hoon-yun/spirngboot-aws.git>
2021-03-16 14:03:24 +0900

    내용 수정

--- src/main/java/com/aws/springboot/Application.java
+++ src/main/java/com/aws/springboot/Application.java
@@ -2,7 +2,7 @@ package com.aws.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing  //JPA Auditing 활성화
 @SpringBootApplication
